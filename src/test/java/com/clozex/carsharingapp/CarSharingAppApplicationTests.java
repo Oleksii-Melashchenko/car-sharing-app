2dcda9d79544cacd823204dfff32dc09b9c81570
clo3eX <pansydaisy83@gmail.com>
2025-01-12 08:02:29 +0100

    initial commit

---
+++ src/test/java/com/clozex/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clozex.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
